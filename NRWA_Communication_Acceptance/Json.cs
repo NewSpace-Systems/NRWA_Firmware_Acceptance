5201eb9cc22dc73cfc888cbbd3e64a0583a2373d
nikeackerman <95609914+nikeackerman@users.noreply.github.com>
2022-12-06 14:17:12 +0200

    Json reading class

---
+++ NRWA_Communication_Acceptance/Json.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NRWA_Communication_Acceptance
+{
+    internal class Json
+    {
+        public static string ReadJsonFile(string jsonFileIn)
+        {
+            string jsonReturn = "";
+            dynamic jsonFile = JsonConvert.DeserializeObject(File.ReadAllText(jsonFileIn));
+
+            JToken creator = jsonFile.SelectToken("creator");
+            jsonReturn += creator.ToString();
+
+            return jsonReturn;
+        }
+
+
+    }
+}
