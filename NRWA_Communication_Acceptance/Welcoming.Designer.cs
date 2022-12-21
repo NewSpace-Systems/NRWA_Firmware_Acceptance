502436d332d800876e6f912ecb66d52d8c4546ae
nikeackerman <95609914+nikeackerman@users.noreply.github.com>
2022-12-21 13:04:21 +0200

    welcoming form

---
+++ NRWA_Communication_Acceptance/Welcoming.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace NRWA_Communication_Acceptance
+{
+    partial class Welcoming
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Welcoming));
+            this.SuspendLayout();
+            // 
+            // Welcoming
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(563, 392);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Welcoming";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Welcoming";
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.Load += new System.EventHandler(this.Welcoming_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ NRWA_Communication_Acceptance/Welcoming.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NRWA_Communication_Acceptance
+{
+    public partial class Welcoming : Form
+    {
+        public Welcoming()
+        {
+            InitializeComponent();
+        }
+
+        private void Welcoming_Load(object sender, EventArgs e)
+        {
+            this.Show();
+            System.Threading.Thread.Sleep(2000);
+            NRWA_FirmVer nRWA_FirmVer = new NRWA_FirmVer();
+            nRWA_FirmVer.Show(this);
+            this.Hide();
+        }
+    }
+}
